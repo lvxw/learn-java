692bc68919b68ac84006017f51059390475c3e8b
lvxw <lvxw@fun.tv>
2018-02-06 11:14:47 +0800

    no message

--- learnLog4j/src/main/java/com/test/learnLog4j/duplication/App.java
+++ learnLog4j/src/main/java/com/test/learnLog4j/duplication/App.java
@@ -6,6 +6,8 @@ public class App {
     private static Logger logger = Logger.getLogger(App.class);
     public static void main(String[] args) {
         System.out.println(123);
-        logger.error("123"); 
+        System.out.println(123);
+        logger.error("123");
+        logger.error("123");
     }
 }
